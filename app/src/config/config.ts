0703eeee12d31d177c32836f22bc6495f355bf40
margazun <oleg.n.fedorov@gmail.com>
2021-11-14 08:57:26 +0700

    add system vars

--- app/src/config/config.ts
+++ app/src/config/config.ts
@@ -4,6 +4,16 @@ import { ConfigT } from "../types/config";
 export function getConfig(name:string): ConfigT {
   const config = rc(name);
   if (!config) {
+    try {
+      return <ConfigT>{
+        bot: {
+          token: <string>process.env.TOKEN,
+          name: <string>process.env.BOTNAME
+        }
+      }
+    } catch (err) {
+      throw new Error(`No envirements.`);
+    }
     throw new Error(`Config by name ${name} not found.`);
   }
   return <ConfigT>config;
